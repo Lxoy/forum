130fe9a1a78b8caa0a91cf8dcfc538648f3ebb83
Lxoy <lo14loyt@gmail.com>
2026-02-17 11:25:07 +0100

    Remove unnecessary comments

--- backend/src/routes/home.routes.js
+++ backend/src/routes/home.routes.js
@@ -2,35 +2,29 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth.middleware');
 
-// Controllers
 const categoryController = require('../controllers/category.controller');
 const threadController = require('../controllers/thread.controller');
 const postController = require('../controllers/post.controller');
 const userController = require('../controllers/user.controller');
 
-/* ---------- CATEGORY ---------- */
 router.get('/categories', authMiddleware, categoryController.getAllCategoriesNames);
 router.post('/categories', authMiddleware, categoryController.createCategory);
 router.delete('/categories/:id', authMiddleware, categoryController.deleteCategory);
 
-/* ---------- FILTERS (MORAJU PRIJE :id) ---------- */
 router.get('/threads/popular', authMiddleware, threadController.getPopularThreads);
 router.get('/categories/:id/threads', authMiddleware, threadController.getThreadsByCategory);
 
-/* ---------- THREAD ---------- */
 router.get('/threads', authMiddleware, threadController.getAllThreads);
 router.post('/threads', authMiddleware, threadController.createThread);
 router.get('/threads/:id', authMiddleware, threadController.getThreadById);
 router.put('/threads/:id', authMiddleware, threadController.updateThreadTitle);
 router.delete('/threads/:id', authMiddleware, threadController.deleteThread);
 
-/* ---------- POSTS ---------- */
 router.get('/threads/:threadId/posts', authMiddleware, postController.getAllPostsByThreadId);
 router.post('/threads/:threadId/posts', authMiddleware, postController.createPost);
 router.put('/posts/:id', authMiddleware, postController.updatePost);
 router.delete('/posts/:id', authMiddleware, postController.deletePost);
 
-/* ---------- USERS ---------- */
 router.get('/users/me', authMiddleware, userController.getProfile);
 router.put('/users/me', authMiddleware, userController.updateProfile);
 router.put('/users/:id/role', authMiddleware, userController.updateUserRole);
